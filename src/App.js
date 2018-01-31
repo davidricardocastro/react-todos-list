7fec483faf362578de7e1c270f5722c8d5f80ce8
David Castro <davidricardocastro@outlook.com>
2018-01-31 15:01:14 +0200

    add / remove todo functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header';
 import TodoInput from './components/todoInput';
+import TodoItem from './components/todoItem';
 
 class App extends Component {
 constructor(props){
@@ -23,12 +24,20 @@ constructor(props){
 }
 
 addTodo(todoText) {
-  console.log("Todo added: ", todoText);
+  let todos = this.state.todos.slice();
+  todos.push({id: this.state.nextId, text: todoText});
+  this.setState({
+    todos: todos,
+    nextId: ++this.state.nextId
+  });
 }
 
-removeTodo(id){
- console.log("removing todo: ", id);
+removeTodo(id) {
+  this.setState({
+      todos: this.state.todos.filter((todo, index) => todo.id !== id)
+    });
 }
+
 
 
   render() {
@@ -37,6 +46,13 @@ removeTodo(id){
         <div className="todo-wrapper">
           <Header />
           <TodoInput todoText="" addTodo={this.addTodo}/>
+          <ul>
+          {
+            this.state.todos.map((todo) => {
+              return <TodoItem todo={todo} key={todo.id} id={todo.id} removeTodo={this.removeTodo}/>
+            })
+          }
+          </ul>
         </div>
 
       </div>
