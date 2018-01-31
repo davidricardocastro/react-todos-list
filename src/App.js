5bac326bf32daa309e48976b01eacc6e584b397b
David Castro <davidricardocastro@outlook.com>
2018-01-31 14:28:10 +0200

    added components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Header from './components/header';
+import TodoInput from './components/todoInput';
 
 class App extends Component {
+constructor(props){
+  super(props);
+
+  this.state = {
+    todos: [
+      {id: 0, text: "make dinner"},
+      {id: 1, text: "buy fruits"},
+      {id: 2, text: "book car maintenance"},
+      {id: 3, text: "cut the grass"}
+    ],
+    nextId:4 
+  }
+
+  this.addTodo = this.addTodo.bind(this);
+  this.removeTodo = this.removeTodo.bind(this);
+}
+
+addTodo(todoText) {
+  console.log("Todo added: ", todoText);
+}
+
+removeTodo(id){
+ console.log("removing todo: ", id);
+}
+
+
   render() {
     return (
       <div className="App">
         <div className="todo-wrapper">
           <Header />
+          <TodoInput todoText="" addTodo={this.addTodo}/>
         </div>
 
       </div>
---
+++ src/components/todoInput.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './todoInput.css';
+
+export default class TodoInput extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { value: "" };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.addTodo = this.addTodo.bind(this);
+
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value});
+    }
+
+    addTodo(todo) {
+
+        if (todo.length > 0) {  //to ensure the todo text is not empty
+            this.props.addTodo(todo);
+            this.setState({ value: '' });
+        }
+
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+                <button className="btn btn-primary" onClick={() => this.addTodo(this.state.value)}> Submit </button>
+            </div>
+        );
+
+    }
+
+}
